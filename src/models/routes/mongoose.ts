a40139feb361ced6d4c2df86b41f5cc9f2ec70e4
Timmy Antonio <timmy.antonio@sseenergyservices.com>
2022-10-24 14:10:45 +0100

    update mongoose.ts

--- src/models/routes/mongoose.ts
+++ src/models/routes/mongoose.ts
@@ -8,10 +8,10 @@ mongoose
     "mongodb+srv://tg-lender:GZSCIeHCWGyrmyZE@cluster0.rwg4svv.mongodb.net/my-products?retryWrites=true&w=majority"
   )
   .then(() => {
-    console.log("Connected to Database!");
+    console.log("Connected to Database...!");
   })
   .catch(() => {
-    console.error("Connection failed!");
+    console.error("Connection failed...!");
   });
 
 export const createProduct = async (req: Request, res: Response, next: any) => {
